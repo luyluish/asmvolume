fa2e83e60371ae15d9e53e3182ee364ab5057f96
Luyluish <143360527+luyluish@users.noreply.github.com>
2024-10-20 02:02:16 -0300

    working release

--- volume.asm
+++ volume.asm
@@ -8,18 +8,24 @@ include \masm32\include\masm32.inc
 includelib \masm32\lib\masm32.lib
 includelib \masm32\lib\kernel32.lib
 includelib \masm32\lib\user32.lib
+include \masm32\include\msvcrt.inc
+includelib \masm32\lib\msvcrt.lib
+include /masm32/macros/macros.asm
 
 .data
 
     ; === Variáveis contendo as solicitações de entrada ===
-    requestName db "Insira o nome do arquivo de entrada:", 0h
-    requestCopy db "Insira o nome do arquivo de saida:", 0h
-    requestVolume db "Insira o valor de reducao de volume:", 0h
+    requestName db "Insira o nome do arquivo de entrada: ", 0h
+    requestCopy db "Insira o nome do arquivo de saida: ", 0h
+    requestVolume db "Insira o valor de reducao de volume: ", 0h
+    requestRepeat db "Deseja alterar outro arquivo? (1 - Confirmar):  "
 
     ; === Variáveis contendo as entradas do usuário ===
     inputName db 50 dup(0)
     inputCopy db 50 dup(0)
     inputVolume db 10 dup(0)
+    inputRepeat db 10 dup(0)
+    repeatNum dw ?
     volumeNum dw ?  ; Armazenar a entrada em formato int
 
     ; === Handles e Contadores ===
@@ -32,16 +38,16 @@ includelib \masm32\lib\user32.lib
     copyCount dd ?
 
     ; === Buffers ===
-    readBuffer dd 16 dup(0)
-    copyBuffer dd 16 dup(0)
+    headerBuffer db 44 dup(?)
+    readBuffer db 16 dup(?)
+    copyBuffer db 16 dup(?)
 
 .code
 
 ; === Função para corrigir as strings inseridas no console ===
 corrigirInput:
-    push ebp        ; ]
-    mov ebp, esp    ; Prólogo da Função
-    sub esp, 8      ; ]
+    push ebp
+    mov ebp, esp
     mov esi, [ebp + 8]
 proximo:
     mov al, [esi]
@@ -52,36 +58,34 @@ proximo:
     dec esi
     xor al, al
     mov [esi], al
-    mov esp, ebp    ; ]
-    pop ebp         ; Epílogo da Função
-    ret 4           ; ]
+    mov esp, ebp
+    pop ebp
+    ret 4
 
 ; === Função para dividir o buffer pela constante inserida pelo usuário ===
 reduzirVolume:
     push ebp
     mov ebp, esp
-    sub esp, 8
-    mov esi, [ebp + 8]  ; Volume
-    mov edi, [ebp + 12] ; Copy
-    mov ebx, [ebp + 16]   ; Read
-    mov ecx, 4
+    mov ebx, [ebp + 8]  ; ReadBuffer
+    mov edi, [ebp + 12] ; CopyBuffer
+    mov esi, DWORD PTR[ebp + 16] ; Volume
+    mov ecx, 8
 reducao:
-    mov eax, [ebx]
+    mov ax, WORD PTR[ebx]
     cwd
-    idiv esi
-    mov [edi], eax
+    idiv si
+    mov WORD PTR[edi], ax
 
-    add edi, 4
-    add ebx, 4
+    add edi, 2
+    add ebx, 2
     
-    cmp ecx, 0
-    je epilogo
     dec ecx
-    jmp reducao
+    cmp ecx, 0
+    jne reducao
 epilogo:
     mov esp, ebp
     pop ebp
-    ret 12
+    ret 10
 
 ; === Início do Programa ===
 start:
@@ -124,23 +128,43 @@ abrir:
     invoke CreateFile, addr inputCopy, GENERIC_WRITE, 0, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL
     mov copyHandle, eax
 
-leitura:
-    ; === Ler os arquivos abertos anteriormente ===
-    invoke ReadFile, readHandle, addr readBuffer, 44, addr readCount, NULL
-    cmp readCount, 0
-    je final
+    ; === Lendo o arquivo inteiro, copiando os 44 primeiros bytes
+    invoke ReadFile, readHandle, addr headerBuffer, 44, addr readCount, NULL
+    invoke WriteFile, copyHandle, addr headerBuffer, 44, addr copyCount, NULL
 
-    ; === Dividir o buffer atual pela constante ===
+leituraDiv:    
+    ; === Lendo o resto do arquivo para reduzir o volume
+    invoke ReadFile, readHandle, addr readBuffer, 16, addr readCount, NULL
     
+    ; === Dividir o buffer atual pela constante (volumeNum) ===
+    push DWORD PTR[volumeNum]
+    push offset copyBuffer
+    push offset readBuffer
+    call reduzirVolume
+    
+    ; === Escrever o buffer processado no arquivo de saída ===
+    invoke WriteFile, copyHandle, addr copyBuffer, 16, addr copyCount, NULL
 
-    ; === Criar arquivo cópia ===
-    invoke WriteFile, copyHandle, addr readBuffer, 44, addr copyCount, NULL
-    jmp leitura
+    cmp readCount, 0
+    jne leituraDiv
 
 final:
     ; === Fechar os handles ===
     invoke CloseHandle, readHandle
     invoke CloseHandle, copyHandle
+
+perguntarOutro:
+    ; === Perguntar se usuário quer repetir ===
+    invoke WriteConsole, outputHandle, addr requestRepeat, sizeof requestRepeat - 1, addr consoleCount, NULL
+    invoke ReadConsole, inputHandle, addr inputRepeat, sizeof inputRepeat, addr consoleCount, NULL
+                                    ; ===================================================
+    push offset inputRepeat         ; Chamada da função para corrigir a resposta
+    call corrigirInput              ; E depois, converter de ASCII para DWORD
+    invoke atodw, addr inputRepeat  ; ===================================================
+    mov repeatNum, ax
+
+    cmp repeatNum, 1
+    je start
 
     invoke ExitProcess, 0
     
